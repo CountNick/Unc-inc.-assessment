3afaf7a48d960655a33e44a1ccc8f4544c1f265b
CountNick <meijer.nick1@gmail.com>
2024-11-10 19:43:46 +0100

    Add page transition

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import { useEffect } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useEffect, useContext } from "react";
+import { Route, Routes, useLocation } from "react-router-dom";
 import WithUIContext from "./context/ui";
 import WithUserContext from "./context/user";
+import { UIContext } from "./context/ui";
 
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
@@ -10,18 +11,19 @@ import Dashboard from "./pages/Dashboard/Dashboard";
 import "./styles/globals.scss";
 
 function App() {
+  const { setMenuIsOpen } = useContext(UIContext);
+
+  const location = useLocation();
+  useEffect(() => {
+    setMenuIsOpen(false);
+    document.startViewTransition();
+  }, [location]);
   return (
-    <WithUserContext>
-      <WithUIContext>
-        <BrowserRouter>
-          <Routes>
-            <Route exact path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-          </Routes>
-        </BrowserRouter>
-      </WithUIContext>
-    </WithUserContext>
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
   );
 }
 
