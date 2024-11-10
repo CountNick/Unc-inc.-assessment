b82aff012c001c362ae322b2e2cc1f11fbb4b920
CountNick <meijer.nick1@gmail.com>
2024-11-10 19:43:46 +0100

    Add page transition

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import WithUserContext from "./context/user";
+import WithUIContext from "./context/ui";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <WithUserContext>
+      <WithUIContext>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </WithUIContext>
+    </WithUserContext>
   </React.StrictMode>
 );
 
