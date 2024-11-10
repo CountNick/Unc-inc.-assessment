f8d71dd50f130663f8a89f93e5e367e9993b2c66
CountNick <meijer.nick1@gmail.com>
2024-11-10 14:44:44 +0100

    Add layout styles

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,13 @@
 import React from "react";
+
+import Layout from "../../Layout/Layout/Layout";
+
 import styles from "./styles.module.scss";
 
 export default function Home() {
-  return <div className={styles["home"]}>Home Page</div>;
+  return (
+    <Layout>
+      <div className={styles["home"]}>Home Page</div>
+    </Layout>
+  );
 }
